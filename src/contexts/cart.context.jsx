a5c8a2959154a9a4a183d896f0248d314e9e4651
elchinovzla <sonnyng.vzla@gmail.com>
2023-01-14 20:06:24 -0700

    Add shopping cart counter

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,13 +1,28 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+import { addCartItem } from "../utils/contexts/cart.util";
 
 export const CartContext = createContext({
     isCartOpen: false,
-    setIsCartOpen: () => { }
+    setIsCartOpen: () => { },
+    cartItems: [],
+    addItemToCart: () => { },
+    cartCount: 0
 })
 
 export const CartProvider = ({ children }) => {
     const [isCartOpen, setIsCartOpen] = useState(false);
-    const value = { isCartOpen, setIsCartOpen };
+    const [cartItems, setCartItems] = useState([]);
+    const [cartCount, setCartCount] = useState(0);
 
+    useEffect(() => {
+        const newCartCount = cartItems.reduce((total, cartItem) => total + cartItem.quantity, 0);
+        setCartCount(newCartCount);
+    }, [cartItems])
+
+    const addItemToCart = (productToAdd) => {
+        setCartItems(addCartItem(cartItems, productToAdd));
+    }
+
+    const value = { isCartOpen, setIsCartOpen, cartItems, addItemToCart, cartCount };
     return <CartContext.Provider value={value}>{children}</CartContext.Provider>
 }
